2a9eafed26451cb847a14b0347c647145177c6e0
Gustavo Frederico Temple Pedrosa <gustavo.temple0@gmail.com>
2019-09-08 14:48:14 -0300

    Adding RabbitTemplates

--- src/main/java/rabbitmq/producer/ProducerApplication.java
+++ src/main/java/rabbitmq/producer/ProducerApplication.java
@@ -1,13 +1,28 @@
 package rabbitmq.producer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import rabbitmq.producer.exchanges.ExchangeOneImpl;
+import rabbitmq.producer.exchanges.ExchangeTwoImpl;
 
 @SpringBootApplication
-public class ProducerApplication {
+public class ProducerApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProducerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerApplication.class, args);
+    }
 
+    @Autowired
+    private ExchangeOneImpl exchangeOne;
+
+    @Autowired
+    private ExchangeTwoImpl exchangeTwo;
+
+    @Override
+    public void run(String... args) throws Exception {
+        exchangeOne.send("test1");
+        exchangeTwo.send("test2");
+    }
 }
